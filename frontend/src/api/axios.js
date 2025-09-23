da1bf811558ba7607575137db33be5f24c649f72
piokub94 <kuberski.piotr94@gmail.com>
2025-09-23 21:44:54 +0200

    .

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_BACKEND_URL + '/api/';
+const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: backendUrl,
 });
 
-// automatyczne dodawanie tokenu
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) config.headers.Authorization = `Token ${token}`;
