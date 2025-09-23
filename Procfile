5a93859cd8ef57fe1eac8f57c5d93512efcd93dd
piokub94 <kuberski.piotr94@gmail.com>
2025-09-23 21:29:51 +0200

    .

--- Procfile
+++ Procfile
@@ -1,4 +1,4 @@
 release: python manage.py migrate --noinput && python manage.py collectstatic --noinput
-web: gunicorn --bind 0.0.0.0:$PORT backend.wsgi
+web: gunicorn backend.wsgi:application --bind 0.0.0.0:$PORT
 worker: celery -A backend worker --loglevel=info --concurrency=2
 beat: celery -A backend beat --loglevel=info
--- backend/settings.py
+++ backend/settings.py
@@ -23,7 +23,9 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1'])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+if not DEBUG:
+    ALLOWED_HOSTS.append(env.str('RAILWAY_PUBLIC_DOMAIN'))
 
 # Application definition
 INSTALLED_APPS = [
